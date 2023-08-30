c697bb7d8712db321cae5618183dfbe8b09614cb
Oleg <oleg@makarenk.ooo>
2023-08-31 02:56:58 +0300

    Fix input_setup_wheel

--- hid-logitech-hidpp.c
+++ hid-logitech-hidpp.c
@@ -3798,6 +3798,7 @@ static int hidpp_input_setup_wheel(struct hid_device *hdev, struct hid_field *fi
 				usage->code == ABS_RZ)) {
 		field->application = HID_GD_MULTIAXIS;
 	}
+	return 0;
 }
 
 static int hidpp_input_mapped(struct hid_device *hdev, struct hid_input *hi,
