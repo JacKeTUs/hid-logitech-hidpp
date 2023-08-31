e8bdac5510a2f44376c365d9f1a929bbf59242f2
Oleg <oleg@makarenk.ooo>
2023-08-31 17:45:48 +0300

    Try to initialize g923 compatibility mode ffb

--- hid-logitech-hidpp.c
+++ hid-logitech-hidpp.c
@@ -4650,7 +4650,7 @@ static const struct hid_device_id hidpp_devices[] = {
 		.driver_data = HIDPP_QUIRK_CLASS_G920 | HIDPP_QUIRK_FORCE_OUTPUT_REPORTS},
 	{ /* Logitech G923 Wheel (Xbox version) over USB */
 	  HID_USB_DEVICE(USB_VENDOR_ID_LOGITECH, USB_DEVICE_ID_LOGITECH_G923_XBOX_WHEEL),
-		.driver_data = HIDPP_QUIRK_CLASS_G920 | HIDPP_QUIRK_FORCE_OUTPUT_REPORTS },
+		.driver_data = HIDPP_QUIRK_CLASS_G920 },
 	{ /* Logitech G PRO Wheel (Xbox version) over USB */
 	  HID_USB_DEVICE(USB_VENDOR_ID_LOGITECH, USB_DEVICE_ID_LOGITECH_G_PRO_XBOX_WHEEL),
 		.driver_data = HIDPP_QUIRK_CLASS_G920 },
